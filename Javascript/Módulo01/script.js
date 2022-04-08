12c8870ded3c813d9cb16424c9d919ea8a0cf150
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-08 11:17:51 -0300

    finalizando módulo01

---
+++ Javascript/Módulo01/script.js
@@ -0,0 +1,11 @@
+var hora = 18;
+
+if ( hora == 12 || hora == 18 ) {
+    console.log("Você está na hora do Rush");
+}
+
+var pessoa = {nome: 'Luis', sobrenome:"Matos"};
+
+pessoa = "Lucas";
+
+console.log(pessoa);
