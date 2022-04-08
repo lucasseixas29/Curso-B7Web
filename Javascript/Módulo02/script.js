3a14ca0cfab27a77c904856d573e54998d3254e8
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-08 13:05:25 -0300

    começando módulo02

---
+++ Javascript/Módulo02/script.js
@@ -0,0 +1,9 @@
+document.getElementById('teste').innerHTML = "Seixas <button> CLIQUE AQUI</button>";
+
+document.getElementsByClassName('lista')[1].innerHTML = 'item alterado';
+
+document.getElementsByTagName('..')
+
+document.getElementsByName('...')
+
+
