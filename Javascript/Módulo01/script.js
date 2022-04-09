5427616ef454fcb411fa42f08dffa359c8445f37
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-08 21:56:52 -0300

    finalizei módulo 01

--- Javascript/Módulo01/script.js
+++ Javascript/Módulo01/script.js
@@ -1,4 +1,17 @@
 var hora = 18;
+if ( hora < 12) {
+console.log("Bom dia");
+} 
+
+else if (hora < 18) {
+    console.log ("Boa tarde");
+}
+
+else if ( hora <= 23) {
+    console.log ("Boa noite");
+}
+
+
 
 if ( hora == 12 || hora == 18 ) {
     console.log("Você está na hora do Rush");
@@ -9,3 +22,14 @@ var pessoa = {nome: 'Luis', sobrenome:"Matos"};
 pessoa = "Lucas";
 
 console.log(pessoa);
+
+var x = 15;
+var y = 15;
+var total = x + y;
+alert(total);
+
+var nome = "Lucas"
+var sobrenome = "Seixas";
+
+var nomecompleto = nome+ ' ' + sobrenome;
+alert(nomecompleto);
