9a7ab9f8fce9b7024871a6594830fbc74a883720
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-10 16:23:57 -0300

    finalizando o módulo 02

--- Javascript/Módulo02/script03.js
+++ Javascript/Módulo02/script03.js
@@ -8,3 +8,23 @@ function pegaranimal() {
     let animal = document.querySelector('.imagem').getAttribute('data-animal');
     alert("O animal é: "+animal);
 }
+
+function subirtela() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function decidirbotaoscroll() {
+    if(window.scrollY == 0) {
+    // ocultar o botão
+    document.querySelector('.scrollbutton').style.display = 'none';
+
+     } else {
+         // mostrar o botão
+         document.querySelector('.scrollbutton').style.display= 'block';
+            }
+}
+
+window.addEventListener('scroll', decidirbotaoscroll);
