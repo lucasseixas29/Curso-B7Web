b6ccddb230362d95241392793bb1ea7ad4e2aa1a
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-10 23:40:51 -0300

    finalizando aula de numbers

---
+++ Javascript/Módulo04/01Strings1.3/script.js
@@ -0,0 +1,12 @@
+let nome = 'Lucas Seixas Matos'
+
+let resultado = nome.split(' ')
+
+
+
+console.log("resultado:", resultado) 
+
+let n = '25.9';
+let res = parseFloat(n) + 5;
+
+console.log(res);
