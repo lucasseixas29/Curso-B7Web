24807cc22a37adc2c461342d31a5376a0081bc3b
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-19 23:55:27 -0300

    finalizando métodos numbers

---
+++ Javascript/Módulo04/Numbers/script.js
@@ -0,0 +1,18 @@
+let n = 10
+
+let res = n.toString()   //para virar string
+console.log(res)
+
+
+/*let n = 10.6585858585
+
+let res = 'R$ '+ n.toFixed(2)
+parseInt()   //Para transformar string em Número e inteiro.
+parseFloat()  // Para manter o número com decimal
+
+console.log(res)*/
+
+/*let n = '25.99'
+
+let res = parseFloat(n) + 5.01
+console.log(res)*/
