cf9d979e57e663e955d884b8bc721d975c715c47
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-20 17:47:42 -0300

    começando aula 4 de array

--- Javascript/Módulo04/metodosarrays/script03.js
+++ Javascript/Módulo04/metodosarrays/script03.js
@@ -1,6 +1,27 @@
 let lista = [45, 4, 9, 16, 25]
+let lista2 = []
 
+/*lista2 = lista.map(function(item){
+    return item * 2
+})*/
 
-let res = lista
+/*lista2 = lista.filter(function(item){
+    if(item>20) {
+        return true
+    } else {
+        return false
+    }
+})*/
+/*lista2 = lista.every(function(item){
+    if(item<46) {
+        return true
+    } else {
+        return false
+    }
+})*/
+
+lista2 = lista.some(function(item){
+    return(item>10)? true : false})
+let res = lista2
 
 console.log(res)
---
+++ Javascript/Módulo04/metodosarrays/script04.js
@@ -0,0 +1,15 @@
+let lista = [
+    {id: 1,nome: 'Lucas',  sobrenome:'Seixas'}
+    {id:2, nome: 'Paulo', sobrenome:'XYZ'}
+    {id:3, nome: 'Carlos', sobrenome: 'Da Silva'}
+]
+
+let lista2 = []
+
+/*lista2 = lista.findIndex(function(item){
+   return (item==16) ? true:false
+})
+
+let res = lista2
+
+console.log(res)*/
