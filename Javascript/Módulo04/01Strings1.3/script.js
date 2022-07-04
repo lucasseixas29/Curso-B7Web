6e5c1f288bd0e793f49fc461e36515a1f6b09c54
lucasseixas29 <lucasseixas29@hotmail.com>
2022-07-04 17:51:56 -0300

    finalizando módulo 03

--- Javascript/Módulo04/01Strings1.3/script.js
+++ Javascript/Módulo04/01Strings1.3/script.js
@@ -1,12 +1,21 @@
-let nome = 'Lucas Seixas Matos'
+/*let nome = 'Lucas Seixas'
 
-let resultado = nome.split(' ')
+let resultado = nome.indexOf(' ')
+
+console.log(resultado)*/
+
+/*let nome = "Lucas Seixas";
+let resultado = nome.substr(-12, -4);
+
+console.log(resultado);*/
 
 
+let nome = "Lucas Seixas"
+let resultado = nome.replace("Seixas","Matos")
 
-console.log("resultado:", resultado) 
+console.log(resultado) 
 
-let n = '25.9';
+/*let n = '25.9';
 let res = parseFloat(n) + 5;
 
-console.log(res);
+console.log(res);*/
